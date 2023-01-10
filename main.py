f60cfb8743c96252d68ded35c8bd3fda702b7ba2
ahuang11 <huang.andrew12@gmail.com>
2023-01-09 21:57:22 -0800

    Add plot

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import datetime
 
 import pandas as pd
 import hvplot.pandas
+import holoviews as hv
 
 pd.set_option("display.max_columns", None)
 
@@ -14,9 +15,8 @@ DATA_URL_FMT = (
     "year2={today.year}&month2={today.month}&day2={today.day}"
 )
 
-def load_data(network: str = "CO_ASOS", station: str = "DEN"):
+def load_data(today: datetime, network: str, station: str):
     """Load data from Iowa Environment Mesonet ASOS."""
-    today = datetime.datetime.today()
     url = DATA_URL_FMT.format(network=network, station=station, today=today)
     df = pd.read_csv(url, parse_dates=True, index_col="day")
     return df
@@ -38,21 +38,66 @@ def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
   	)
 
 
+
+
+def select_data(
+    today: datetime,
+    preprocess_df: pd.DataFrame
+) -> pd.DataFrame:
+    """
+    Select only the rows matching "todate" (day of year).
+    """
+    return preprocess_df.loc[
+        (preprocess_df.index.month == today.month) &
+        (preprocess_df.index.day == today.day)
+    ]
+
+
+def plot_data(
+    today: datetime,
+    select_df: pd.DataFrame,
+    weather_var: str
+):
+    """
+    Create a histogram showing the distribution of
+    weather over the years, and highlight today's
+    weather with a red line.
+    """
+    # prepare the titles and labels
+    year_range = (
+      f"{select_df.index.year.min()} - "
+      f"{select_df.index.year.max()}"
+    )
+    weather_label = weather_var.replace("_", " ").title()
+    title = (
+      f"{network}: {station}'s {year_range} "
+      f"{today:%B %d}'s {weather_label}"
+    )
+    climo_hist = select_df.hvplot.hist(
+      weather_var,
+      title=title,
+      xlabel=weather_label,
+      ylabel="Number of Years"
+    )
+
+    # highlight today
+    weather_today = select_df.iloc[-1][weather_var]
+    today_line = hv.VLine(weather_today).opts(color="red")
+    today_text = hv.Text(
+      weather_today, 3, f"Today"
+    ).opts(text_align="right")
+    weather_plot = climo_hist * today_line * today_text
+    return weather_plot
+
+
 if __name__ == "__main__":
-    df = load_data()
-    # optionally find out what the bad value is
-    # by sorting the unique values;
-    # the bad values usually show up as
-    # the first or last value of the list
-    print(sorted(df["max_dewpoint_f"].unique()))
+    # parameters
+    network = "CO_ASOS"
+    station = "DEN"
+    today = datetime.datetime.today()
 
+    df = load_data(today=today, network=network, station=station)
     preprocess_df = preprocess_data(df)
-
-    # show that converting to float reduced the memory footprint
-    # from 13125856 to 1138408 bytes
-    print("Before", df.memory_usage(deep=True).sum())
-    print("After", preprocess_df.memory_usage(deep=True).sum())
-
-    # show statistics of the dataframe
-    print(preprocess_df.describe())
-  
+    select_df = select_data(today, preprocess_df)
+    weather_plot = plot_data(today, select_df, "min_temp_f")
+    display(weather_plot)
